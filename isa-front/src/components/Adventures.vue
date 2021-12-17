a41ee3cbcbb1983132bef6473192c0ee52f9ca16
milicaoui <vumilicaamm@gmail.com>
2021-12-17 01:26:22 +0100

    feat: added search and sort card

--- isa-front/src/components/Adventures.vue
+++ isa-front/src/components/Adventures.vue
@@ -43,10 +43,58 @@
     </div>
 </section>
 
+<div class="card text-white bg-dark ml-100 mb-100" style="max-width: 20rem; position: fixed;bottom: 10px; right: 10px">
+  <div class="card-header">Search or sort adventures</div>
+  <div class="card-body">
+  <label>Chose search category:</label>
+
+
+  <div class="dropdown mt-3">
+    <button class="btn btn-secondary dropdown-toggle" type="button"  id="dropdown1" data-bs-toggle="dropdown">
+        Adventure name
+    </button>
+    <ul class="dropdown-menu" aria-labelledby="dropdown1">
+        <li><a class="dropdown-item">Action</a></li>
+        <li><button class="dropdown-item">Another action</button></li>
+        <li><button class="dropdown-item">Something else here</button></li>
+    </ul>
+  </div>
+
+  
+
+ <div class="input-group mt-2">
+    <input type="text" class="form-control" aria-label="Search adventures...">
+      <button class = "btn btn-primary btn-lg">Search</button>
+    </div>
+ </div>
+
+ <div class="card-body">
+  <label>Chose sort category:</label>
+  
+<br>
+<div class="input-group">
+    <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdown2" data-bs-toggle="dropdown" aria-expanded="true">
+            Adventure name
+        </button>
+        <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdown2">
+            <li><a class="dropdown-item active" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Separated link</a></li>
+        </ul>
+    </div>
+    <button class = "btn btn-primary">Sort</button>
+ </div>
+ </div>
+</div>
+
 </template>
 
 <script>
 import axios from 'axios'
+import Popper from 'popper.js'
 export default{
     name: 'adventures',
     data(){
@@ -76,7 +124,8 @@ export default{
                'promoDescription' : 'promo opis',
                'avgGrade' : 'ocjena'
            }
-        ]
+        ],
+        searchCategoryClicked: true
         }
     },
     methods:{
--- isa-front/src/main.js
+++ isa-front/src/main.js
@@ -2,11 +2,13 @@ import * as Vue from 'vue'
 //import axios from "axios"
 //import { createWebHistory, createRouter } from 'vue-router';
 import Axios from "axios";
+//mport 'bootstrap/scss/bootstrap.scss';
 import "bootstrap/dist/css/bootstrap.min.css"
 import { createApp } from 'vue'
 import router from './router'
 import store from './store'
 import App from './App.vue'
+
 
 
 
