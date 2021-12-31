07a3ec85f5676c9d7b00de36e4fdd75128fa9f71
Andrea Sabo Cibolja <andrea_saboc@uns.ac.rs>
2021-12-31 02:52:55 +0100

    feat: single boat view

--- isa-front/src/components/BoatView.vue
+++ isa-front/src/components/BoatView.vue
@@ -1,16 +1,56 @@
 <template>
- Boat view
+<div class="boat-view">
+  <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner top-slider">
+      <div class="carousel-item active">
+        <img src="../assets/yacht.jpg" class=" w-100 h-25" alt="../assets/yacht.jpg">
+      </div>
+      <div class="carousel-item">
+        <img src="../assets/yacht.jpg" class="d-block w-100 h-25  " alt="../assets/yacht.jpg">
+      </div>
+      <div class="carousel-item">
+        <img src="../assets/yacht.jpg" class="d-block w-100 h-25" alt="../assets/yacht.jpg">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+  <p class="fs-5">
+    {{}}
+  </p>
 
+</div>
 
 </template>
 
 <script>
 export default {
-  name: "BoatView"
+  name: "BoatView",
+  data: function (){
+    boatToShow: 
+  }
 }
 
 
 </script>
 
 <style scoped>
+.boat-view{
+  horiz-align: center;
+  width: 70%;
+  margin-left: 15%;
+  background-color: #bbbbbb;
+  height: 100%;
+}
+
+.boat-view .top-slider{
+  height: 250px;
+
+}
 </style>
