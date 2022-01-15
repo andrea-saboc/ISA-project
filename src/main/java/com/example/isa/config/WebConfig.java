2a2a1c0868fa2064be5ad8f4970581fed88898b3
milicaoui <vumilicaamm@gmail.com>
2022-01-15 04:23:27 +0100

    fix: getting images config fixed

--- src/main/java/com/example/isa/config/WebConfig.java
+++ src/main/java/com/example/isa/config/WebConfig.java
@@ -4,11 +4,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+	
+	
+	@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){ 
+            registry.addResourceHandler("/**")
+                 .addResourceLocations("classpath:/static/")
+                 .addResourceLocations("classpath:/public/");
+    }
+    
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
@@ -19,4 +29,9 @@ public class WebConfig implements WebMvcConfigurer {
 			}
 		};
 	}
+	
+	
+
+	
+	
 }
