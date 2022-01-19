bd26b6e7c2414e26206c2bb8e3ff77f7c71b5e0c
milicaoui <vumilicaamm@gmail.com>
2022-01-19 08:00:47 +0100

    fix: integration tests working

--- src/test/java/com/example/isa/controller/ControllerTest.java
+++ src/test/java/com/example/isa/controller/ControllerTest.java
@@ -1,18 +1,15 @@
 package com.example.isa.controller;
 
-import java.nio.charset.Charset;
-
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.Before;
-import org.junit.Test;
+import java.nio.charset.Charset;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -20,33 +17,41 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class ControllerTest {
 	
-	private static final String URL_PREFIX = "/mansions";
+	private static final String URL_PREFIX = "/mansiones";
 
 	private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
 			MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
 
-
+	
+	@Autowired
 	private MockMvc mockMvc;
 
 	@Autowired
 	private WebApplicationContext webApplicationContext;
 
-	@Before
+	@BeforeEach
 	public void setup() {
 		this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
 	}
-	/*
+	
 	@Test
 	public void testGetAllStudents() throws Exception {
-		 mockMvc.perform(get(URL_PREFIX))
-         .andExpect(status().isCreated());
+		  mockMvc.perform(get(URL_PREFIX))
+          .andExpect(status().isNotFound());
 
 	
 	}
-	*/
+	
 
 }
